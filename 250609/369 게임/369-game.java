25ab638c1476ab5d8f42bc2c87bd64ebdca9513e
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-06-09 17:01:06 +0900

    Title: 369 게임, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250609/369 게임/369-game.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = 1; i <= N; i++) {
+            
+            if(i%3 == 0) {
+                System.out.print(0 + " ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
