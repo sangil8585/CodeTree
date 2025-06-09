0ba8fcff4aa5d324bcd345e868572f042f77d81c
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-06-09 17:08:27 +0900

    Title: 369 게임, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250609/369 게임/369-game.java
+++ 250609/369 게임/369-game.java
@@ -7,8 +7,9 @@ public class Main {
         int N = sc.nextInt();
 
         for(int i = 1; i <= N; i++) {
+            String num = Integer.toString(i);
             
-            if(i%3 == 0 || i%10 == 3 || i%10 == 6 || i%10 == 9) {
+            if(i%3 == 0 || num.contains("3") || num.contains("6") || num.contains("9")) {
                 System.out.print(0 + " ");
             } else {
                 System.out.print(i + " ");
