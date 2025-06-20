23dfd75cb4a969f3e0b26f57b51ba5aaf67c8e33
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-06-20 18:50:05 +0900

    Title: 코로나 메뉴얼2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250620/코로나 메뉴얼2/covid-manual2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = 0, b = 0, c = 0, d = 0; 
+
+        for(int i = 0; i < 3; i++) {
+            String chk = sc.next();
+            int temp = sc.nextInt();
+            
+            if(chk.equals("Y") && temp > 36 ) {
+                a++;
+            } else if (chk.equals("N") && temp > 36) {
+                b++;
+            } else if (chk.equals("Y") && temp <= 36) {
+                c++;
+            } else {
+                d++;
+            }
+            System.out.print(a + " " + b + " " + c + " " + d);
+        }
+
+        if (a >= 2) {
+            System.out.print(" E");
+        }
+    }
+}
