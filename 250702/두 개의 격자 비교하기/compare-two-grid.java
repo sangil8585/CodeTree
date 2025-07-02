b99e6307e3d12db4b737f63f31d3308c54313daa
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-07-02 11:18:34 +0900

    Title: 두 개의 격자 비교하기, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

--- 250702/두 개의 격자 비교하기/compare-two-grid.java
+++ 250702/두 개의 격자 비교하기/compare-two-grid.java
@@ -8,7 +8,6 @@ public class Main {
         int M = sc.nextInt();
         int[][] arr1 = new int[N][M];
         int[][] arr2 = new int[N][M];
-        int[][] arr3 = new int[N][M];
 
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < M; j++) {
@@ -24,8 +23,7 @@ public class Main {
 
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < M; j++) {
-                arr3[i][j] = (arr1[i][j] == arr2[i][j]) ? 0 : 1;
-                System.out.print(arr3[i][j] + " ");
+                System.out.print(((arr1[i][j] == arr2[i][j]) ? 0 : 1) + " ");
             }
             System.out.println();
         }
