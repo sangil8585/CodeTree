4f11e416e125ecd2df0360290b7f923c893cf068
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-07-09 15:37:01 +0900

    Title: 공백을 기준으로 출력, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250709/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        System.out.print(str1.replace(" ", ""));
+        System.out.print(str2.replace(" ", ""));
+    }
+}
