3b93cc2a42dbe940992c880066eb60593e226e9b
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-07-10 12:47:02 +0900

    Title: 문자열에서 문자 출력, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250710/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.next();
+        for(int i = 0; i < n.length(); i++) {
+            System.out.println(n.charAt(i));
+        }
+    }
+}
