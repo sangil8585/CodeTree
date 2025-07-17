9dfb28b987cd8a2a2bb1fa912539a7d3d68e85f7
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-07-17 11:16:45 +0900

    Title: 부분문자열 위치 구하기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 250717/부분문자열 위치 구하기/find-location-of-substring.java
+++ 250717/부분문자열 위치 구하기/find-location-of-substring.java
@@ -7,15 +7,6 @@ public class Main {
         String a = sc.next();
         String b = sc.next();
 
-        int cnt = -1;
-        
-        for (int i = 0; i <= a.length() - b.length(); i++) {
-            if (a.substring(i, i + b.length()).equals(b)) {
-                cnt = i;
-                break;
-            }
-        }
-        
-        System.out.print(cnt);
+        System.out.print(a.indexOf(b));
     }
 }
