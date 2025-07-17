9f9914603fa6c006490096fc21450c72b583678e
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-07-17 10:56:02 +0900

    Title: 특정 문자의 등장 횟수, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250717/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        int cnt1 = 0;
+        int cnt2 = 0;
+        
+        for (int i = 0; i < a.length() - 1; i++) {
+            if (a.substring(i, i + 2).equals("ee")) {
+                cnt1++;
+            }
+        }
+        for (int i = 0; i < a.length() - 1; i++) {
+            if (a.substring(i, i + 2).equals("eb")) {
+                cnt2++;
+            }
+        }
+        
+
+        System.out.print(cnt1 + " " + cnt2);
+        
+    }
+}
