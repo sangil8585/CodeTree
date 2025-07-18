51f2332110bfc316994c73ac753963a6d6a76a5a
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-07-18 23:35:26 +0900

    Title: 문자 교체하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250718/문자 교체하기/changing-char.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        b = a.subString(0, 1);
+        System.out.print(b);
+        
+    }
+}
