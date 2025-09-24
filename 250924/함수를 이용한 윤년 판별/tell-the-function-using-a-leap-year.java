dd447a8c7c49858c09147fe36628669210ff6d4f
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-09-24 16:00:54 +0900

    Title: 함수를 이용한 윤년 판별, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250924/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 250924/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -15,12 +15,7 @@ public class Main {
     }
 
     public static boolean check(int year) {
-        if ( year % 4 == 0) {
-            if(year % 100 == 0 && year % 400 != 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return (year % 100 != 0 && year % 4 == 0) || (year % 400 == 0);
+        
     }
 }
