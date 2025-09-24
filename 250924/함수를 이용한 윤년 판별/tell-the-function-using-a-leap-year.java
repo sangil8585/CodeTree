e6ccb150b2ff2049f13fcc5e77653dcd4ecc381a
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-09-24 15:54:47 +0900

    Title: 함수를 이용한 윤년 판별, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250924/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        // 4로 나눠떨어지는해 윤년 2000, 2020, 2008 등..
+        // 100으로 나눠 떨어지면 평년임 1900
+        
+        if(check(y)) {
+            System.out.print(true);
+        } else {
+            System.out.print(false);
+        }
+    }
+
+    public static boolean check(int year) {
+        if ( year % 4 == 0) {
+            if(year % 100 == 0 && year % 400 != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
