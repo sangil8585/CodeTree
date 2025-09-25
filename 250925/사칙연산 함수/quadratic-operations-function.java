3b26e3aa22236450e72daae76e637844b09dbf64
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-09-25 11:23:58 +0900

    Title: 사칙연산 함수, Time: 117ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250925/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char o = sc.next().charAt(0);
+        int c = sc.nextInt();
+        
+        int sum = 0;
+
+        switch(o) {
+            case '+' :
+                sum = plus(a, c);
+                break;
+            case '-' :
+                sum = min(a, c);
+                break;
+            case '*' :
+                sum = gop(a, c);
+                break;
+            case '/' :
+                sum = nanu(a, c);
+                break;
+            default:
+                System.out.print("False");
+                return;
+        }
+    }
+
+    public static int plus(int x, int y) {
+        return x + y;
+    }
+    public static int min(int x, int y) {
+        return x - y;
+    }
+    public static int gop(int x, int y) {
+        return x * y;
+    }
+    public static int nanu(int x, int y) {
+        return x / y;
+    }
+}
