d16df8359eaacfa7b339866151b807ae0466a1ae
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-11-19 12:36:47 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251119/재귀함수를 이용한 별 출력/star-output-with-recursive-function.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        num(n);
+    }
+
+    public static void num(int n) {
+        if(n == 0) {
+            return;
+        }
+
+        num(n-1);
+        for(int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        
+    }
+}
