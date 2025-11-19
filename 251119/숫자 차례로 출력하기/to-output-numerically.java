2583e469304a17cf847a6633d5021642ea69a86a
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-11-19 12:28:25 +0900

    Title: 숫자 차례로 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251119/숫자 차례로 출력하기/to-output-numerically.java
+++ 251119/숫자 차례로 출력하기/to-output-numerically.java
@@ -3,20 +3,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        printUp(1, n);   
+        num1(n);
         System.out.println();
-        printDown(n);    
+        num2(n);
     }
 
-    public static void printUp(int cur, int n) {
-        if(cur > n) return;
-        System.out.print(cur + " ");
-        printUp(cur + 1, n);
+    public static void num1(int n) {
+        if(n == 0) {
+            return;
+        }
+        num1(n-1);
+        System.out.print(n+" ");
     }
 
-    public static void printDown(int n) {
-        if(n == 0) return;
-        System.out.print(n + " ");
-        printDown(n - 1);
+    public static void num2(int n) {
+        if(n == 0) {
+            return;
+        }
+        System.out.print(n+" ");
+        num2(n-1);
     }
 }
