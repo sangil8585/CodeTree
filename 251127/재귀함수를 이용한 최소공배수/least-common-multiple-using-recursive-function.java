8edeb6cc624d9d30688e3cca9eca9b9f0aef4abc
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-11-27 12:31:53 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251127/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        // 최소공배수 = 두 자연수의 곱 / 최대공약수
+
+        // 최대 공약수
+        System.out.println(result(arr, 0));
+    }
+
+    // 최대공약수
+    private static int gcd(int a, int b) {
+        if(b == 0) return a;
+
+        return gcd(a, a%b);
+    }
+
+    // 최소공배수
+    private static int lcm(int a, int b) {
+        return (a * b) / gcd(a,b);
+    }
+
+    private static int result(int[] arr, int num) {
+        if(num == arr.length-1) {
+            retrun arr[num];
+        }
+        // 인덱스 arr[0], arr[1]을 gcd(arr[0], arr[1])로 돌리고 돌린다.
+        // arr[0], arr[1]의 값을 더한다 => plusNum
+        // plusMun / gcd(arr[0], arr[1])
+        return lcm(arr[num], result(arr, num+1));
+    }
+}
