7008f81f6177101c80d862fc1323e6cf551e3c63
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-12-19 00:32:38 +0900

    Title: 야바위, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251219/야바위/ya-rock.java
+++ 251219/야바위/ya-rock.java
@@ -5,20 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         
-        boolean[] tf = new boolean[n + 1];
+        int[] a = new int[n];
+        int[] a = new int[n];
         
         for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int c = sc.nextInt();
-            tf[c] = true;
-        }
-        
-        for (int i = 1; i <= n; i++) {
-            if (!tf[i]) {
-                System.out.println(i);
-                break;
+            
+            if (ball == a) {
+                ball = b;
+            } else if (ball == b) {
+                ball = a;
             }
         }
+        
+        System.out.println(ball);
+        sc.close();
     }
 }
