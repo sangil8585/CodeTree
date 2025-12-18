1afa67332b4206f414d4ee7354b848bb65d702b4
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2025-12-19 00:39:58 +0900

    Title: 야바위, Time: 150ms, Memory: 11MB, Status: Passed - Codetree

--- 251219/야바위/ya-rock.java
+++ 251219/야바위/ya-rock.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,21 +6,35 @@ public class Main {
         int n = sc.nextInt();
         
         int[] a = new int[n];
-        int[] a = new int[n];
+        int[] b = new int[n];
+        int[] c = new int[n];
         
         for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            
-            if (ball == a) {
-                ball = b;
-            } else if (ball == b) {
-                ball = a;
-            }
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+            c[i] = sc.nextInt();
         }
         
-        System.out.println(ball);
-        sc.close();
+        int result = 0;
+
+
+        for (int num = 1; num <= 3; num++) {
+            int cup = num;
+            int score = 0;
+
+            for (int i = 0; i < n; i++) {
+                if(cup == a[i]) {
+                    cup = b[i];
+                } else if(cup == b[i]) {
+                    cup = a[i];
+                }
+
+                if(cup == c[i]) {
+                    score++;
+                }
+            }
+            result = Math.max(result, score);
+        }
+        System.out.println(result);
     }
 }
