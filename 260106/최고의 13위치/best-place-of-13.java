4ff38a546eefa0cdd2c7af865d4f64a9be4edb66
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2026-01-06 22:39:55 +0900

    Title: 최고의 13위치, Time: 162ms, Memory: 11MB, Status: Passed - Codetree

--- 260106/최고의 13위치/best-place-of-13.java
+++ 260106/최고의 13위치/best-place-of-13.java
@@ -1,26 +1,32 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
-    public static final int MAX_N = 20;
-
-    public static int n;
-    public static int[][] arr = new int[MAX_N][MAX_N];
-    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        // Please write your code here.
 
-        n = sc.nextInt();
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                arr[i][j] = sc.nextInt();
+        int ans = 0;
 
-        int maxCnt = 0;
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n - 2; j++) {
-                int cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
-                maxCnt = Math.max(maxCnt, cnt);
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j + 2 < n; j++) {
+                int sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
+                if (sum > ans) ans = sum;
             }
-        
-        System.out.println(maxCnt);
+        }
+
+        for (int i = 0; i + 2 < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = grid[i][j] + grid[i + 1][j] + grid[i + 2][j];
+                if (sum > ans) ans = sum;
+            }
+        }
+
+        System.out.println(ans);
     }
+
+    
 }
