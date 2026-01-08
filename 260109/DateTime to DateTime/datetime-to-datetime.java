6ab09162686069030f0281a02925dcbcf797d238
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2026-01-09 00:04:29 +0900

    Title: DateTime to DateTime, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 260109/DateTime to DateTime/datetime-to-datetime.java
+++ 260109/DateTime to DateTime/datetime-to-datetime.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        
-        int diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11);
-        
-        if(diff < 0)
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int day = 11;
+        int hour = 11;
+        int min = 11;
+
+        int startDay = day * 24 * 60 + hour * 60 + min;
+        int endDay = A * 24 * 60 + B * 60 + C;
+
+        if(startDay > endDay) {
             System.out.print(-1);
-        else
-            System.out.print(diff);
+            return;
+        }
+
+        System.out.print(endDay - startDay);
     }
 }
