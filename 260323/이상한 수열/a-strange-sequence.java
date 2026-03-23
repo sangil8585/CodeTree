a3c44dc05d303ce37a6ff0fc0e4db7f8a8762e0a
sang-il Byun <72428954+sangil8585@users.noreply.github.com>
2026-03-23 23:35:06 +0900

    Title: 이상한 수열, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 260323/이상한 수열/a-strange-sequence.java
+++ 260323/이상한 수열/a-strange-sequence.java
@@ -11,6 +11,7 @@ public class Main {
         if(n == 2) return 2;
 
         return add(n/3) + add(n-1);
+        
         // n번째 수는 n/3 수와 n-1 번째 수의 합으로 나열된 수열
         // 3 = 3/3 + (3-1) 
     }
